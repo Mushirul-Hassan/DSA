0445e2eac97cdbee1349d7ea249759a617f68ef0
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-06-04 17:24:14 +0530

    moving forward

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -1,42 +1,88 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void print1(int n) {
-    for(int i =0;i<n;i++){
-        for(int j=0;j<n;j++){
-         cout << "* ";
-        }
-        cout << endl;
-    }
-}
-
-void print2(int n){
-     for(int i =0;i<n;i++){
-        for(int j=0;j<=i;j++){
+void print1(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
             cout << "* ";
         }
         cout << endl;
     }
 }
 
-void print3(int n){
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            cout << j << " " ;
+void print2(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "* ";
         }
         cout << endl;
     }
 }
 
-// void print4(int n){
-//     for (int i=1;i<=n;i++){
-//         for(int j=n;j<=2*n;j++){
-//             cout << j << " ";
-//         }
-//         cout << endl;
-//     }
-// }
-int main() {
-    int n;
-    cin >> n;
-    print3(n);
- }
+void print3(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print4(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << i << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print5(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+void print6(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n - i + 1; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print7(int n){
+     for (int i= )
+}
+int main()
+{
+    int t;
+    cin >> t; 
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin >> n;
+        print7(n);
+    }
+}
