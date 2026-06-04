845bd01ac3173d3d4cdc92d19111c686362733dc
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-06-04 12:14:30 +0530

    continuing patterns....

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -9,12 +9,34 @@ void print1(int n) {
     }
 }
 
-int main() {
-    int t;
-    cin >> t;
-    for (int i = 0; i<t;i++){
-        int n;
-        cin >> n;
-        print1(n);
+void print2(int n){
+     for(int i =0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            cout << "* ";
+        }
+        cout << endl;
     }
 }
+
+void print3(int n){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            cout << j << " " ;
+        }
+        cout << endl;
+    }
+}
+
+// void print4(int n){
+//     for (int i=1;i<=n;i++){
+//         for(int j=n;j<=2*n;j++){
+//             cout << j << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+int main() {
+    int n;
+    cin >> n;
+    print3(n);
+ }
