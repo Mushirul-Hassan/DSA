801a02eb9534924c3db0d0c24281064a20140de4
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-06-12 12:34:35 +0530

    started again with and will continue...

--- patterns/patterns.cpp
+++ patterns/patterns.cpp
@@ -148,6 +148,101 @@ void print11(int n)
         cout << endl;
     }
 }
+
+void print12(int n)
+{
+    int space = 2 * (n - 1);
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j;
+        }
+
+        // space
+        for (int j = 1; j <= space; j++)
+        {
+            cout << " ";
+        }
+
+        for (int j = i; j >= 1; j--)
+        {
+            cout << j;
+        }
+
+        cout << endl;
+        space -= 2;
+    }
+}
+
+void print13(int n)
+{
+    int num = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << num << " ";
+            num = num + 1;
+        }
+        cout << endl;
+    }
+}
+
+void print14(int n)
+{
+    for (int i = 0; i <= n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + i; ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print15(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (char ch = 'A'; ch <= 'A' + (n - i - 1); ch++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print16(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        char ch ='A' + i;
+        for (int j = 0; j<=i; j++)
+        {
+            cout << ch << " ";
+        }
+        cout << endl;
+    }
+}
+
+void print17(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << "-";
+        }
+     
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << "-";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
     int t;
@@ -156,7 +251,7 @@ int main()
     {
         int n;
         cin >> n;
-        print11(n);
+        print16(n);
         // print8(n);
     }
 }
