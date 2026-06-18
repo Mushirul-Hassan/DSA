664479b6c56cc4a93fa28c863543bf92190dffbc
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-06-18 12:47:22 +0530

    reverse done

---
+++ math/reverse.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int count(int n)
+{
+    int rev = 0;
+    while (n > 0)
+    {
+        int lst = n % 10;
+        rev = (rev * 10) + lst;
+        n = n / 10;
+    }
+    return rev;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << count(n);
+}
