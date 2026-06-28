58d3f549f7812937a2ab310ee29f2283070f0536
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-06-28 13:30:33 +0530

    insertion done

---
+++ sorting/insertion.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+void insSort(int n, int arr[])
+{
+   for(int i = 1 ;i < n;i++) {
+
+    int k = arr[i];
+    int j =  i -1 ;
+    while (j >= 0 && arr[j] > k){
+        arr[j + 1] = arr[j];
+        j--;
+      
+    }
+    arr[j + 1] = k;
+
+   }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+     insSort(n, arr);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+   
+    return 0;
+}
