6aa3fbf8c7dd83d6d7aa0c1d0de4ee5d26c18e76
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-06-29 15:48:50 +0530

    started arrays

---
+++ array/largest-num.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int largest(int arr[],  int n)
+{
+    int max = arr[0];
+ for ( int i=0; i < n; i++ ){
+    if ( arr[i] > max){
+        max = arr[i];
+    }
+    
+ }
+ return max;
+
+   
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int ans = largest(arr, n);
+   cout << ans << endl;
+    
+   
+    return 0;
+
+   
+}
