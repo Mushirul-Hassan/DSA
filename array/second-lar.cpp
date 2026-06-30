9a4b54fd5b1351829ea2011caeac78409a87dca6
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-06-30 13:54:23 +0530

    second-largest better approach

---
+++ array/second-lar.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int sLargest(int arr[], int n)
+{
+    int max1 = arr[0];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max1)
+        {
+            max1 = arr[i];
+        }
+    }
+     int max2 = arr[-1];
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > max2 && arr[i] != max1)
+        {
+            max2 = arr[i];
+        }
+    }
+
+    return max2;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int ans = sLargest(arr, n);
+    cout << ans << endl;
+
+    return 0;
+}
