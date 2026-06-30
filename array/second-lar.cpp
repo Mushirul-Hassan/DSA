2493e61385c2759ef7e216b67954981df755048e
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-06-30 14:03:08 +0530

    optimal

--- array/second-lar.cpp
+++ array/second-lar.cpp
@@ -3,21 +3,20 @@ using namespace std;
 int sLargest(int arr[], int n)
 {
     int max1 = arr[0];
+     int max2 = arr[-1];
     for (int i = 0; i < n; i++)
     {
         if (arr[i] > max1)
         {
+            max2 = max1;
             max1 = arr[i];
         }
-    }
-     int max2 = arr[-1];
-    for (int i = 0; i < n; i++)
-    {
-        if (arr[i] > max2 && arr[i] != max1)
-        {
+        else if ( arr[i] > max2 && arr[i] < max1){
             max2 = arr[i];
         }
     }
+    
+   
 
     return max2;
 }
