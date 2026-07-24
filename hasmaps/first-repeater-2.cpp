84e77052b2886caf8857894685ed89d3aa6caf26
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-07-24 14:52:38 +0530

    first repeater but check and then insert

---
+++ hasmaps/first-repeater-2.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+void printF(int n, int arr[])
+{
+
+  unordered_map<int, int> freq;
+
+  for (int i = 0; i < n; i++)
+  {
+
+    if (freq.find(arr[i]) != freq.end())
+    {
+      cout << arr[i];
+        break;
+    }
+    freq[arr[i]] = i;
+  
+    // else
+    // {
+    //   freq[arr[i]] = i;
+    // }
+  }
+}
+
+int main()
+{
+  int n;
+
+  cin >> n;
+
+  int arr[n];
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+
+  printF(n, arr);
+}
