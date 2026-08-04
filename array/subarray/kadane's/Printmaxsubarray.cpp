c54f7b9b65ca72f9de2e22e90a8b3eafb24406a0
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-04 15:02:51 +0530

    working on the extended part of the max subarray sum , i.e. to print the subarray also

---
+++ array/subarray/kadane's/Printmaxsubarray.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int maxSubarray(vector<int> &nums, int n)
+{
+    int best = nums[0];
+    int ans = nums[0];
+    int start = 0;
+    int bestStart = 0;
+    int bestEnd  = 0;
+    for (int i = 1; i < n; i++)
+    {
+        int v1 = best + nums[i];
+        int v2 = nums[i];
+        best = max(v1, v2);
+        ans = max(ans, best);
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> nums(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> nums[i];
+    }
+
+    cout << maxSubarray(nums, n);
+
+    return 0;
+}
