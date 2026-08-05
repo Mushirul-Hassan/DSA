ad2e6cc2784067e6b8680ef65de4256f03330af0
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-05 10:14:20 +0530

    "printed" the max sum subarray , using indices

--- array/subarray/kadane's/Printmaxsubarray.cpp
+++ array/subarray/kadane's/Printmaxsubarray.cpp
@@ -1,20 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
-int maxSubarray(vector<int> &nums, int n)
+vector<int> maxSubarray(vector<int> &nums, int n)
 {
     int best = nums[0];
     int ans = nums[0];
     int start = 0;
     int bestStart = 0;
-    int bestEnd  = 0;
+    int bestEnd = 0;
     for (int i = 1; i < n; i++)
     {
         int v1 = best + nums[i];
         int v2 = nums[i];
-        best = max(v1, v2);
-        ans = max(ans, best);
+        // best = max(v1, v2);
+        if (v2 > v1)
+        {
+            best = v2;
+            start = i;
+        }
+        else
+        {
+            best = v1;
+        }
+        // ans = max(ans, best);
+        if (best > ans)
+        {
+            ans = best;
+            bestStart = start;
+            bestEnd = i;
+        }
     }
-    return ans;
+    vector<int> arr;
+    for (int i = bestStart; i <= bestEnd; i++)
+    {
+        arr.push_back(nums[i]);
+    }
+    return arr;
 }
 
 int main()
@@ -27,7 +47,11 @@ int main()
         cin >> nums[i];
     }
 
-    cout << maxSubarray(nums, n);
+    vector<int> result = maxSubarray(nums, n);
 
+    for (int x : result)
+    {
+        cout << x << " ";
+    }
     return 0;
 }
