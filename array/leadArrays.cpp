f0ec91b6c1011dba4ea45574892db23128449981
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-10 11:26:27 +0530

    moving forward

--- array/leadArrays.cpp
+++ array/leadArrays.cpp
@@ -1,24 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
-	vector<int> leaders(vector<int>& arr, int n) {
-		// int r = n-1;
-		int l = -1;
-		
-		for (int i = n - 1; i > 0; i--) {
-			if (arr[i] < arr[i - 1]) {
-				// r = arr[i];
-                l = i-1;
-			}
-		}
-		vector<int>leadArr;
-		for (int i = 0; i<n; i++) {
-			leadArr.push_back(arr[l]);
-		}
-		return leadArr;
-	}
+vector<int> leaders(vector<int> &arr, int n)
+{
+    // int r = n-1;
+    vector<int> leadArr;
+    // int l = -1;
 
+    for (int i = 0; i <= n-1; i++)
+    {
+        if (arr[i] > arr[i + 1])
+        {
+            // r = arr[i];
+            // l = i;
+            leadArr.push_back(arr[i]);
+        }
+    }
 
-    int main()
+    return leadArr;
+}
+
+int main()
 {
     int n;
     cin >> n;
