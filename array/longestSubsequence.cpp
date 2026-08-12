383a944b16b30b8e46bbf977ca73caa292157e0c
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-12 13:57:12 +0530

    finally done with a brute method, will now optimize it

--- array/longestSubsequence.cpp
+++ array/longestSubsequence.cpp
@@ -1,36 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-int longestConsecutive(vector<int> &arr, int n)
+int longestConsecutive(vector<int> &nums, int n)
 {
-    int seq = 0;
-    int maxSeq = 0;
-    sort(arr.begin(), arr.end());
-    for (int i = 0; i <= n; i++)
+    int seq = 1;
+    int i = 0;
+    int maxSeq = 1;
+    sort(nums.begin(), nums.end());
+    for (int j = 1; j < n; j++)
     {
-        if (arr[i + 1] - arr[i] == 1)
+        if (nums[j] - nums[i] == 1)
         {
             seq++;
+            i++;
+        }
+        else if (nums[j] == nums[i])
+        {
+            i = j;
+        }
+        else
+        {
+            seq = 1;
+            i++;
         }
         maxSeq = max(maxSeq, seq);
     }
-    return maxSeq + 1;
+    return n != 0 ? maxSeq : 0;
 }
 
 int main()
 {
     int n;
     cin >> n;
-    vector<int> arr(n - 1);
+    vector<int> nums(n - 1);
     for (int i = 0; i < n; i++)
     {
-        cin >> arr[i];
+        cin >> nums[i];
     }
 
-    cout << longestConsecutive(arr, n);
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
-    }
+    cout << longestConsecutive(nums, n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << nums[i] << " ";
+    // }
 
     return 0;
 }
