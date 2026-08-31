c4ffa2ccf06afeea1b111f1c0bf1e4e8427d9e84
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-08-31 13:11:49 +0530

    attempt floor in sorted array

---
+++ array/binary-search/floor.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int floor(vector<int> &arr, int n, int target)
+{
+    int l = 0;
+    int r = n - 1;
+    int ans = -1;
+    while (l <= r)
+    {
+        int mid = (l + r) / 2;
+        if (arr[mid] <= target)
+        {
+            ans = mid;
+            r = mid - 1;
+        }
+        else
+        {
+            l = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    int n, target;
+    cin >> n >> target;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << floor(arr, n, target);
+
+    return 0;
+}
