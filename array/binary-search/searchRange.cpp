245effc1fadf85a0df52b57b6313a16fb8bf7eb6
Mushirul Hassan <mushirulhassan2@gmail.com>
2026-09-02 23:33:03 +0530

    reaching somewhere

--- array/binary-search/searchRange.cpp
+++ array/binary-search/searchRange.cpp
@@ -4,15 +4,29 @@ using namespace std;
 vector<int> searchRange(vector<int> &nums, int target, int n)
 {
 
-    vector<int> pos;
-    for (int i = 0; i < n; i++)
-    {
-        if (nums[i] == target)
-        {
-            pos.push_back(i);
+     int l = 0;
+        int r = n - 1;
+        int ans = n;
+
+        //    vector<int> empty;
+        //    empty.push_back([-1,-1]);
+        vector<int> pos;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                ans = mid;
+                pos.push_back(mid);
+                r = mid - 1;
+                // } else if (nums[mid] > target) {
+                //     r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
         }
-    }
-    return pos;
+        // return pos.size() != 0 ? pos : null;
+        sort(pos.begin(), pos.end());
+        // return pos.size() != 0 ? pos : [ -1, -1 ];
+        return pos;
 }
 
 int main()
